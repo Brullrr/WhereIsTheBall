ccce06050743976976882e95758a80176f169925
Brullrr <rbrull@yahoo.com>
2021-10-24 16:43:27 +0900

    Updates

---
+++ precache-manifest.cdb612a9f1d55439fa9dbdcf8b122e34.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "176c1fb11f639261bcc604b9907a0973",
+    "url": "/WhereIsTheBall/index.html"
+  },
+  {
+    "revision": "00c0e7c4fb2424774ba8",
+    "url": "/WhereIsTheBall/static/css/main.0efaa39e.chunk.css"
+  },
+  {
+    "revision": "75f1f8e8b4d9650b2fed",
+    "url": "/WhereIsTheBall/static/js/2.d0d12121.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WhereIsTheBall/static/js/2.d0d12121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c0e7c4fb2424774ba8",
+    "url": "/WhereIsTheBall/static/js/main.2d2993f7.chunk.js"
+  },
+  {
+    "revision": "0388142bcd1b1e325053",
+    "url": "/WhereIsTheBall/static/js/runtime-main.2fce4988.js"
+  },
+  {
+    "revision": "08c48d16abc5deb297c8b68225597e89",
+    "url": "/WhereIsTheBall/static/media/background.08c48d16.jpg"
+  },
+  {
+    "revision": "899a8a3408028b2bdf35401d6541ed64",
+    "url": "/WhereIsTheBall/static/media/bison.899a8a34.png"
+  },
+  {
+    "revision": "5b7abe3242e93055d0c41bbd065ce591",
+    "url": "/WhereIsTheBall/static/media/box.5b7abe32.png"
+  },
+  {
+    "revision": "c609f968adb6b241b827c7c174325f32",
+    "url": "/WhereIsTheBall/static/media/door.c609f968.jpg"
+  }
+]);
